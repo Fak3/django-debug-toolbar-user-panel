08c83617468156b13e5f7a96e22c6d19fcf6b49c
Ilya Baryshev <baryshev@gmail.com>
2013-05-08 18:34:07 +0400

    django-debug-toolbar-user-panel-fc==0.1

--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
-
 from distutils.core import setup
 
 setup(
-    name='django-debug-toolbar-user-panel',
-    description="Panel for the Django Debug toolbar to quickly switch between "
-        "users.",
-    version='0.1',
-    url='http://code.playfire.com/',
+    name='django-debug-toolbar-user-panel-fc',
+    description="Panel for the Django Debug toolbar to quickly switch between users.",
+    version='1.0',
+    url='https://github.com/futurecolors/django-debug-toolbar-user-panel',
 
     author='Playfire.com',
     author_email='tech@playfire.com',
